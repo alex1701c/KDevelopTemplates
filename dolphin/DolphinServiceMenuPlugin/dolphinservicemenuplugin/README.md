79c8f26b4ce09910d79fb0797ee639680b57ed00
alex1701c <alex1701c.dev@gmx.net>
2020-02-16 12:28:29 +0100

    Add dolphin plugin template

---
+++ dolphin/DolphinServiceMenuPlugin/dolphinservicemenuplugin/README.md
@@ -0,0 +1,22 @@
+# Dolphin Service Menu Plugin
+
+This project is a plugin for dolphin which provides service menus.
+
+To build this plugin run:
+```
+mkdir -p build
+cd build
+cmake ..
+# With Makefiles
+make -j$(nproc)
+sudo make install
+# or with ninja
+ninja -j$(nproc)
+sudo ninja install
+```
+
+To create a more optimized release build run instead of
+`cmake ..` `cmake -DCMAKE_BUILD_TYPE=Release ..`
+
+The plugin can be disabled in Configure Dolphin -> Services -> uncheck %{APPNAME} 
+or uninstalled by running `sudo make uninstall` or `sudo ninja uninstall` from the build directory
---
+++ dolphin/DolphinServiceMenuPlugin/package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+mkdir -p ~/.local/share/kdevappwizard/template_descriptions/
+mkdir -p ~/.local/share/kdevappwizard/templates/
+
+# This automatically updates/installs the template in your local templates folder
+if [[ $(basename "$PWD") == "DolphinServiceMenuPlugin" ]];then
+    mkdir -p build
+    rm -rf build/*
+    cd build
+    cmake ..
+    make
+    /bin/cp ../dolphinservicemenuplugin/dolphinservicemenuplugin.kdevtemplate ~/.local/share/kdevappwizard/template_descriptions/
+    /bin/cp dolphinservicemenuplugin.tar.bz2 ~/.local/share/kdevappwizard/templates/
+else
+    echo "Please go to the project root"
+fi
